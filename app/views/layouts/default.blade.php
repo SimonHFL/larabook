2697fc5d002003035fff2ddfb35a6c1209008650
Simon Flachs <simon@hivelocity.co.jp>
2014-12-05 17:38:44 +0900

    The master page

---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" >
+</head>
+<body>
+
+    <div class="container">
+        @yield('content')
+    </div>
+    <scripts src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></scripts>
+
+</body>
+</html>
