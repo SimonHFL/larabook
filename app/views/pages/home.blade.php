e6fae6e2a729217464b353712f477c5062ef0aef
Simon Flachs <simon@hivelocity.co.jp>
2014-12-10 12:37:56 +0900

    [8] start

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -5,7 +5,7 @@
         <h1>Welcome to Larabook!</h1>
         <p>Welcome to the premeir place to talk about laravel with others. Why don't you sign up to see what all the fuss is about?</p>
         <p>
-          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Sign Up!</a>
+            {{ link_to_route('register_path', 'Sign Up!', NULL ,['class' => 'btn btn-lg btn-primary']) }}
         </p>
       </div>
 
