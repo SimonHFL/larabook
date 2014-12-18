e4e8b2e5f596cacdcfeb61333706845faea5937c
Simon Flachs <simon@hivelocity.co.jp>
2014-12-18 12:01:36 +0900

    [11] start

--- app/controllers/RegistrationController.php
+++ app/controllers/RegistrationController.php
@@ -1,14 +1,22 @@
 <?php
 
 use Larabook\Forms\RegistrationForm;
+use Larabook\Registration\RegisterUserCommand;
+use Larabook\Core\CommandBus;
 
-class RegistrationController extends \BaseController {
+class RegistrationController extends BaseController {
+
+    use CommandBus;
 
     /**
      * @var RegistrationForm
      */
     private $registrationForm;
 
+    /**
+     * @param CommandBus $commandBus
+     * @param RegistrationForm $registrationForm
+     */
     function __construct(RegistrationForm $registrationForm)
     {
         $this->registrationForm = $registrationForm;
@@ -33,8 +41,10 @@ class RegistrationController extends \BaseController {
     {
         $this->registrationForm->validate(Input::all());
 
-        $user = User::create(
-            Input::only('username', 'email', 'password')
+        extract(Input::only('username','email','password'));
+
+        $user = $this->execute(
+            new RegisterUserCommand($username, $email, $password)
         );
 
         Auth::login($user);
